59571213ebffd43422ebcc6dd62ecdaa11a6f63c
EhsanOrandi <ehsanorandi17@gmail.com>
2020-12-23 18:45:39 +0330

    Implementing model of account and product

---
+++ accounts/models.py
@@ -0,0 +1,119 @@
+from django.contrib import auth
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('Email Address'), unique=True)
+    first_name = models.CharField(_("First Name"), max_length=150, null=True)
+    last_name = models.CharField(_("Last Name"), max_length=150, null=True)
+    mobile= models.CharField(_("Mobile"), max_length=13, null=True)
+    avatar = models.ImageField(_('Avatar'), upload_to = 'user/avatar', null=True, blank=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
+    def clean(self):
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        """
+        Return the user full_name.
+        """
+        return "{} {}".format(self.first_name, self.last_name)
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
+
+    def __str__(self):
+        self.get_full_name()
+
+
+class Email(models.Model):
+    to = models.ForeignKey(User, related_name="email_to", on_delete=models.CASCADE)
+    subject = models.CharField(_("Subject"), max_length=150)
+    body = models.TextField(_("Body"))
+
+    class Meta:
+        verbose_name = _("Email")
+        verbose_name_plural = _("Emails")
+
+    def __str__(self):
+        return self.to
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    city = models.CharField(_("City"), max_length=150)
+    street = models.CharField(_("Street"), max_length=150)
+    alley = models.CharField(_("Alley"), max_length=150, null=True, blank=True)
+    zip_code = models.CharField(_("Zip Code"), max_length=10, unique=True)
+
+    class Meta:
+        verbose_name = _("Address")
+        verbose_name_plural = _("Addresses")
+
+    def __str__(self):
+        return self.zip_code
+
+class Shop(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(_("Name"), max_length=150, unique=True, db_index=True)
+    slug = models.SlugField(_("Slug"), db_index=True, unique=True)
+    description = models.TextField(_("Description"))
+    image = models.ImageField(_("Image"), upload_to="shop/image", null=True, blank=True)
+
+    class Meta:
+        verbose_name = _("Shop")
+        verbose_name_plural = _("Shops")
+
+    def __str__(self):
+        return self.name
+
---
+++ products/models.py
@@ -0,0 +1,114 @@
+from __future__ import unicode_literals
+from django.utils.translation import ugettext_lazy as _     # for making app multilingual
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.conf import settings
+User = get_user_model()
+
+# Create your models here.
+class Category (models.Model) :
+    name = models.CharField(_("Name"), max_length=125)
+    slug = models.SlugField(_("Slug"), unique=True, db_index=True)
+    details = models.TextField(_("Details"), null=True, blank=True)
+    image = models.ImageField(_("Image"), upload_to='categoty/image', null=True)
+    parent = models.ForeignKey('self', verbose_name=_("Parent"), on_delete=models.SET_NULL, null=True, blank=True, related_name='children', related_query_name='children')
+    
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+
+    def __str__(self):
+        return self.slug
+
+
+class Brand (models.Model) :
+    name = models.CharField(_("Name"), max_length=150)
+    slug = models.SlugField(_("Slug"), db_index=True, unique=True)
+    details = models.TextField(_("Details"), null=True, blank=True)
+    image = models.ImageField(_("Image"), upload_to="brand/image", null=True)
+
+    class Meta:
+        verbose_name = _("Brand")
+        verbose_name_plural = _("Brands")
+
+    def __str__(self):
+        return self.slug
+
+
+class Product (models.Model) :
+    name = models.CharField(_("Name"), max_length=150)
+    slug = models.SlugField(_("Slug"), db_index=True, unique=True)
+    details = models.TextField(_("Details"), null=True, blank=True)
+    image = models.ImageField(_("Image"), upload_to="product/main_image", null=True)
+    brand = models.ForeignKey(Brand, verbose_name=_("Brand"), on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, verbose_name=_("Category"), on_delete=models.SET_NULL, null=True, blank=True)
+
+    class Meta:
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
+
+    def __str__(self):
+        return self.slug
+
+class ProductMeta(models.Model):
+    label = models.CharField(_("Label"), max_length=100)
+    value = models.CharField(_("Value"), max_length=100)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name=_("Product"))
+
+
+class ShopProduct (models.Model) :
+    price = models.IntegerField(_("Price"))
+    quantity = models.IntegerField(_("Quantity"))
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    shop = models.ForeignKey('accounts.Shop', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _("Shop Product")
+        verbose_name_plural = _("Shop Products")
+
+
+class Image (models.Model) :
+    image = models.ImageField(_("Image"), upload_to="product/image")
+    product = models.ForeignKey(Product, related_name="product", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _("Image")
+        verbose_name_plural = _("Images")
+
+    def __str__(self):
+        pass
+
+
+
+class Comment (models.Model) :
+    text = models.TextField(_("Text"))
+    rate = models.IntegerField(_("Rate"))
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
+    user = models.ForeignKey(User, verbose_name=_("user"), on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name=_("product"), related_name='comments', related_query_name='comments', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _("Comment")
+        verbose_name_plural = _("Comments")
+
+    def __str__(self):
+        return self.text
+
+    # @property
+    # def like_count(self):
+    #     return Comment_like.objects.filter(comment=self, status=True).count()
+
+    # @property
+    # def dislike_count(self):
+    #     return Comment_like.objects.filter(comment=self, status=False).count()
+
+
+class off (models.Model):
+    pass
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        pass
