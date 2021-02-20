f8810f92f9625cac8c8825507f8d276286b95d1b
EhsanOrandi <ehsanorandi17@gmail.com>
2021-02-20 23:36:12 +0330

    bug fix

--- online_market/settings.py
+++ online_market/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'asset')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR, 'assets']
+STATICFILES_DIRS = [BASE_DIR, 'static']
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
